108634d0509e563bfa9823aa8350f050116057ce
ali-vakili <ali.vakili.tb@gmail.com>
2023-11-30 19:07:06 +0330

    update supabaseTypes.ts

--- src/lib/types/supabaseTypes.ts
+++ src/lib/types/supabaseTypes.ts
@@ -9,7 +9,7 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
-      _AutoGalleryToAutoGalleryCategory: {
+      _AutoGalleryToAutoGalleryAndCarCategory: {
         Row: {
           A: string
           B: number
@@ -24,17 +24,17 @@ export interface Database {
         }
         Relationships: [
           {
-            foreignKeyName: "_AutoGalleryToAutoGalleryCategory_A_fkey"
+            foreignKeyName: "_AutoGalleryToAutoGalleryAndCarCategory_A_fkey"
             columns: ["A"]
             isOneToOne: false
             referencedRelation: "AutoGallery"
             referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "_AutoGalleryToAutoGalleryCategory_B_fkey"
+            foreignKeyName: "_AutoGalleryToAutoGalleryAndCarCategory_B_fkey"
             columns: ["B"]
             isOneToOne: false
-            referencedRelation: "AutoGalleryCategory"
+            referencedRelation: "AutoGalleryAndCarCategory"
             referencedColumns: ["id"]
           }
         ]
@@ -259,7 +259,7 @@ export interface Database {
         }
         Relationships: []
       }
-      AutoGalleryCategory: {
+      AutoGalleryAndCarCategory: {
         Row: {
           abbreviation: string | null
           category: string
@@ -292,9 +292,25 @@ export interface Database {
         }
         Relationships: []
       }
+      BuildYear: {
+        Row: {
+          id: number
+          year: string
+        }
+        Insert: {
+          id?: number
+          year: string
+        }
+        Update: {
+          id?: number
+          year?: string
+        }
+        Relationships: []
+      }
       Car: {
         Row: {
-          build_year: number
+          build_year_id: number
+          category_id: number
           createdAt: string
           description: string
           fuel_type_id: number
@@ -306,7 +322,8 @@ export interface Database {
           updatedAt: string
         }
         Insert: {
-          build_year: number
+          build_year_id: number
+          category_id: number
           createdAt?: string
           description: string
           fuel_type_id: number
@@ -318,7 +335,8 @@ export interface Database {
           updatedAt: string
         }
         Update: {
-          build_year?: number
+          build_year_id?: number
+          category_id?: number
           createdAt?: string
           description?: string
           fuel_type_id?: number
@@ -330,6 +348,20 @@ export interface Database {
           updatedAt?: string
         }
         Relationships: [
+          {
+            foreignKeyName: "Car_build_year_id_fkey"
+            columns: ["build_year_id"]
+            isOneToOne: false
+            referencedRelation: "BuildYear"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "Car_category_id_fkey"
+            columns: ["category_id"]
+            isOneToOne: false
+            referencedRelation: "AutoGalleryAndCarCategory"
+            referencedColumns: ["id"]
+          },
           {
             foreignKeyName: "Car_fuel_type_id_fkey"
             columns: ["fuel_type_id"]
@@ -798,3 +830,83 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
