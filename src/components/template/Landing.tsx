c58da210e9d31656a9a17058a25de2fa242e2075
ali-vakili <ali.vakili.tb@gmail.com>
2023-12-14 23:19:36 +0330

    edit the landing page style

--- src/components/template/Landing.tsx
+++ src/components/template/Landing.tsx
@@ -18,14 +18,14 @@ import comments from "@/constants/comments.json"
 const Landing = () => {
   return (
     <>
-      <section id="hero" className="grid grid-cols-2 md:grid-cols-12 desktop:min-h-[540px] min-h-[480px] items-center container px-4">
+      <section id="hero" className="grid grid-cols-2 md:grid-cols-12 desktop:min-h-[540px] min-h-[480px] items-center px-5">
         <div className="space-y-3 md:col-start-2 md:col-span-5 col-span-1">
           <h1 className="text-4xl md:text-5xl font-bold" style={{lineHeight: "1.2"}}>Empower Your <br /> Auto Gallery Seamlessly</h1>
           <p className="text-lg">Manage, showcase, rent, and sell cars in one place</p>
           <p>or someone seeking to rent or purchase the perfect car, our platform provides seamless solutions for both showcasing and acquiring vehicles. Explore, rent, or buy with ease!</p>
         </div>
       </section>
-      <section id="actions" className="container pb-10">
+      <section id="actions" className="px-5 md:px-8 pb-10">
         <div className="flex items-start desktop:flex-nowrap flex-wrap gap-4">
           <Link href={"#"} className={`${buttonVariants({ variant: "default", size: "lg" })} group h-48 rounded-lg flex-col flex-grow basis-2/6 py-8 !text-lg !items-start !justify-start`}>
             Rental Cars
@@ -47,21 +47,21 @@ const Landing = () => {
       <section id="brands" className="py-12 space-y-14">
         <h3 className="text-2xl font-semibold text-muted-foreground text-center">Supported Brands</h3>
         <Marquee speed={30} gradient={true} gradientColor={"#FAFAFA"} gradientWidth={140} >
-          <Image className="mx-16" src={bmwLogo} alt="bmw-logo"/>
-          <Image className="mx-16" src={ferrariLogo} alt="ferrari-logo"/>
-          <Image className="mx-16" src={nissanNextLogo} alt="nissan-next-logo"/>
-          <Image className="mx-16" src={teslaLogo} alt="tesla-logo"/>
-          <Image className="mx-16" src={mercedesBenzLogo} alt="mercedes-benz-logo"/>
-          <Image className="mx-16" src={lamborghiniLogo} alt="lamborghini-logo"/>
-          <Image className="mx-16" src={renaultLogo} alt="renault-logo"/>
-          <Image className="mx-16" src={audiLogo} alt="audio-logo"/>
-          <Image className="mx-16" src={volkswagenLogo} alt="volkswagen-logo"/>
+          <Image className="mx-20" src={bmwLogo} alt="bmw-logo"/>
+          <Image className="mx-20" src={ferrariLogo} alt="ferrari-logo"/>
+          <Image className="mx-20" src={nissanNextLogo} alt="nissan-next-logo"/>
+          <Image className="mx-20" src={teslaLogo} alt="tesla-logo"/>
+          <Image className="mx-20" src={mercedesBenzLogo} alt="mercedes-benz-logo"/>
+          <Image className="mx-20" src={lamborghiniLogo} alt="lamborghini-logo"/>
+          <Image className="mx-20" src={renaultLogo} alt="renault-logo"/>
+          <Image className="mx-20" src={audiLogo} alt="audio-logo"/>
+          <Image className="mx-20" src={volkswagenLogo} alt="volkswagen-logo"/>
         </Marquee>
         <div className="bg-secondary w-fit mx-auto rounded-full p-6 px-8">
           <h4 className="text-lg font-medium">And <span className="text-white bg-primary p-1.5 rounded-full">+50</span></h4>
         </div>
       </section>
-      <section id="comments" className="py-12 container">
+      <section id="comments" className="py-12 px-5 md:px-8">
         <Image className="mx-auto mb-8" src={quote} alt="quote"/>
         <p className="mx-auto max-w-4xl text-lg text-muted-foreground text-center mb-24">Our mission is to empower auto gallery agents with seamless inventory management and sales tools while offering prospective buyers and renters a hassle-free experience to explore, rent, or purchase their ideal car. Simplifying transactions and expanding choices, we bridge the gap between agents and seekers, revolutionizing the automotive industry.</p>
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 gap-y-12 mb-10">
@@ -82,7 +82,7 @@ const Landing = () => {
           ))}
         </div>
       </section>
-      <footer className="container py-6 border-t">
+      <footer className="py-6 px-5 md:px-8 border-t">
         <p className="text-muted-foreground">© {new Date().getFullYear()} autzon. All rights reserved.</p>
       </footer>
     </>
